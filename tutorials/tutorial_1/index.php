6169ca65a741552357c154b88071ab782ecfc542
EaintThayaphyOo <88219986+EaintThayaphyOo@users.noreply.github.com>
2022-02-16 21:09:29 +0630

    [master] add a tutorial_1

--- tutorials/tutorial_1/index.php
+++ tutorials/tutorial_1/index.php
@@ -7,6 +7,22 @@
   <title>Chess Board</title>
 </head>
 <body>
-  
+  <h1>Chess Board</h1>
+  <table width="350px" cellspacing="0px" cellpadding="0px" border="1px">
+    <?php
+    for ($row = 1; $row <=8; $row++) {
+      echo "<tr>";
+      for ($col = 1; $col <= 8; $col++) {
+        $sum = $row + $col;
+        if ($sum % 2 == 0) {
+          echo "<td height=35px width=35px bgcolor=#FFFFFF></td>";
+        } else {
+          echo "<td hright=35px width=35px bgcolor=#000000></td>";
+        }
+        }
+        echo "</tr>";
+    }
+    ?>
+  </table>
 </body>
 </html>
