764bdb310e2fe5c7c72591178e123d445b7b2ae6
EaintThayaphyOo <88219986+EaintThayaphyOo@users.noreply.github.com>
2022-02-27 17:28:52 +0630

    [master] uploads reviews

--- tutorials/tutorial_7/index.php
+++ tutorials/tutorial_7/index.php
@@ -19,10 +19,10 @@
     if(isset($_POST['generate_text'])){
         $text=$_POST['qr_text'];
         $folder = $_POST['folder'];
-        $dirPath = ''.$folder;
+        /*$dirPath = ''.$folder;
         if(!is_dir($dirPath)){
             mkdir($dirPath);
-        }
+        }*/
         $file_name1="qr.png";
         $file_name= $dirPath. "/" .$file_name1;
         QRcode::png($text,$file_name);
